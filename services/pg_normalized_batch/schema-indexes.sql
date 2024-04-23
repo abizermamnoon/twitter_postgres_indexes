3aa125fed6151ab5e72969a8b36f10aea21b7c74
Abizer <abizermmamnoon@gmail.com>
2024-04-23 09:43:30 -0700

    passes tests on terminal

---
+++ services/pg_normalized_batch/schema-indexes.sql
@@ -0,0 +1,8 @@
+SET max_parallel_maintenance_workers = 60;
+SET max_parallel_workers = 60;
+SET maintenance_work_mem TO '4 GB';
+create index on tweet_tags(id_tweets, tag);
+create index on tweet_tags(tag, id_tweets);
+create index on tweets(id_tweets, lang);
+create index on tweets(lang);
+CREATE INDEX idx_tweets_text_gin ON tweets USING GIN(to_tsvector('english', text)) where lang = 'en';
